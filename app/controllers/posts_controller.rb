912e438255ad89b636f5b418119453aab4dfca20
yoshi23s <classa1990@gmail.com>
2022-08-07 21:46:18 +0900

    投稿画像保存まで

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,13 @@ class PostsController < ApplicationController
   def index
     @post = Post.new
     @room = Room.find(params[:room_id])
-    @posts = @room.posts.includes(:user)
+    @posts = Post.all.order("created_at DESC")
+
   end
 
   
   def new
     @post = Post.new
-    logger.debug params
     @room_id = params[:room_id]
     @room = Room.find(params[:room_id])
     # @posts = @room.posts.includes(:user)
@@ -23,11 +23,21 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  
+  def show
+    @post = Post.find(params[:id])
+
+  end
+
 
   private
   def post_params
     params.require(:post).permit(:image,:post_title, :post_info, :room_id).merge(user_id: current_user.id)
   end
+
+  # def set_post
+  #   @post = Post.find(params[:id])
+  # end
 
   #   params.require(:post).permit(:content).merge(user_id: current_user.id)
   #   params.require(:post).permit(:content, :image).merge(user_id: current_user.id)
